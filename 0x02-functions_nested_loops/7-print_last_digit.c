4d476f02d1113187d854c61f33c164cff9d76673
Caroline Mutune <carolmutune@gmail.com>
2022-12-14 12:47:01 -0800

    Fibonacci

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,17 +10,16 @@
  */
 int print_last_digit(int n)
 {
-	int 1;
-
-	1 = n % 10;
-	if (1 < 0)
+	int l;
+	l = n % 10;
+	if (l < 0)
 	{
-		_putchar(-1 + 48);
-		return (-1);
+		_putchar(-l + 48);
+		return (-l);
 	}
 	else
 	{
-		_putchar(1 + 48);
-		return (1);
+		_putchar(l + 48);
+		return (l);
 	}
 }
